1fad057af8942defd38a48c17ce5f9b825025daf
wangfulong <wangfulong@flowcapai.com>
2021-11-16 11:41:19 +0800

    update

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const Vue = require('vue');
-const server = require('express')();
+const Vue = require("vue");
+const server = require("express")();
 
-const template = require('fs').readFileSync('./index.template.html', 'utf-8');
+const template = require("fs").readFileSync("./index.template.html", "utf-8");
 
-const renderer = require('vue-server-renderer').createRenderer({
+const renderer = require("vue-server-renderer").createRenderer({
   template,
 });
 
 const context = {
-  title: 'vue ssr',
+  title: "vue ssr",
   metas: `
-    <meta name="keyword" content="vue.ssr">
-    <meta name="description" content="vue ssr demo"> 
+  <link rel="canonical" href="https://www.baidu.com" />
+  <meta property="fb:app_id" content="264197305525058" />
+<meta property="og:url" content="https://www.baidu.com" />
+<meta property="og:title" content="your_link_title">
+<meta property="og:image" content="https://rockflow-web-template.oss-cn-hongkong.aliyuncs.com/email-template/captcha-en.png">
   `,
 };
+server.get("/a/b", (req, res) => {
+  res.send(200);
+});
 
-server.get('*', (req, res) => {
+server.get("*", (req, res) => {
   const app = new Vue({
     data: {
       url: req.url,
@@ -25,11 +31,11 @@ server.get('*', (req, res) => {
   renderer.renderToString(app, context, (err, html) => {
     console.log(err);
     if (err) {
-      res.status(500).end('Internal Server Error');
+      res.status(500).end("Internal Server Error");
       return;
     }
     res.end(html);
   });
 });
 
-server.listen(8080);
+server.listen(8000);
